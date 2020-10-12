fda39dcf601dcb52a892378905d85ff7e52b5434
Sayan Biswas <poorprogrammer-python@users.noreply.github.com>
2020-10-12 20:40:43 +0530

    Updates

---
+++ precache-manifest.c25738787917eff79f71dc1cf9caf08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fdf7e73a5e3056eb7256eedf1fd1f8",
+    "url": "/recipe-app-reactjs/index.html"
+  },
+  {
+    "revision": "f307e2d38af6a8fb1fcf",
+    "url": "/recipe-app-reactjs/static/css/main.6140309c.chunk.css"
+  },
+  {
+    "revision": "b5ba15c5c28934f55ad5",
+    "url": "/recipe-app-reactjs/static/js/2.4b37d867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app-reactjs/static/js/2.4b37d867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f307e2d38af6a8fb1fcf",
+    "url": "/recipe-app-reactjs/static/js/main.0655142b.chunk.js"
+  },
+  {
+    "revision": "9967ec403691da88d2be",
+    "url": "/recipe-app-reactjs/static/js/runtime-main.4a315883.js"
+  }
+]);
